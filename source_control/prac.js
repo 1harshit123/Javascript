691c7a08c761db444670b96ef601614d96ab23ad
Harshit malviya <144029518+1harshit123@users.noreply.github.com>
2024-10-12 11:22:37 +0000

    trulty.js done

--- source_control/prac.js
+++ source_control/prac.js
@@ -1,1 +1,27 @@
-console.log("Harshit")
+// if(2<="7"){
+//     console.log("executed");
+// }
+// // In js if we use "==" operator, it will check only value not datatype 
+// // if we want to check datatype then we have to use "==="
+// const score=10;
+// if(score<40){
+//     const power =80;
+//     console.log(`is full ${power}`);
+// }
+// console.log(`is full ${power}`);
+// If we use var here instead of const because var can be accessable outside the scope also
+// use of var is not suggested in larger programs
+// if(score<20) console.log("harshit malviya");
+// if(score<20) console.log("harshit malviya"),console.log(" malviya") ; but use of this should be prohibited
+// switch case syntax is same as c++
+// const value=1;
+// switch(value){
+//     case 1:
+//         console.log("case one matched");
+//         break;
+//     case 2:
+//         console.log("case 2 matched");
+//         break;
+//     default:
+//         console.log("default case");
+// }
---
+++ source_control/truthly.js
@@ -0,0 +1,33 @@
+// const userEmail="harshit@123";
+// const userEmail="";
+
+// if(userEmail){
+//     console.log("truth");
+// } else{
+//     console.log("falsy");
+// }
+// Falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+// Truly values
+// "0", "false", " ", [], {}, function(){}
+
+// const emptyObj={}
+// if (Object.keys(emptyObj).length===0) {
+//     console.log("Object is empty");
+// }
+// other true conditions
+// false==0
+// false==''
+// 0==''
+
+// Nullish Coalescing Operator(??): null undefined
+
+// let val1;
+// // val1=5 ?? 20;
+// // val1=null ?? 10;
+// val1=undefined ?? 50;
+// console.log(val1);
+
+// Terniary operator
+// condition ? true : false
+ 
